63126a4c9fcdc0a76c817036829fd940b6ca0df8
Muhammad ALi <161317819+WazirAly07@users.noreply.github.com>
2025-12-26 22:52:41 +0500

    upload on cloudiney code done successfull thank you

--- src/controllers/registerUser.controller.js
+++ src/controllers/registerUser.controller.js
@@ -1,10 +1,87 @@
 import { asyncHandler } from "../utils/asynHandler.js";
+import {apiError} from "../utils/apiError.js";
+import {User} from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import {apiResponse} from "../utils/apiResponse.js";   
 
 const registerUser = asyncHandler(async (req, res, next) =>{
-    return res.status(201).json({
-        success: true,
-        message: "User registered successfully"
-    });
+    const {username,fullname, email, password} = req.body;
+    console.log(username, email, password);
+    if(
+        [username, fullname, email, password].some((files)=>
+        fields?.trim() ===""))
+    {
+        throw new apiError(
+            400, "All fields are required"
+        );
+    }
+    const existingUser = await User.findOne({
+        $or: [{username},{email}]
+    })
+    if(existingUser){
+        throw new apiError(409, "username or email already exits");
+    }
+    const avatarLocalPath = req.files?.avatar[0].path;
+    const coverImageLocalPath = req.files?.coverImage[0].path;
+    if(!avatarLocalPath){
+        throw new apiError(400, "Avatar is required");
+    }
+    const avatar = await uploadToCloudinary(avatarLocalPath)
+    const coverImage = await uploadToCloudinary(coverImageLocalPath)
+    if(!avatar){
+        throw new apiError(500, "Error while uploading avatar image");
+    }
+    const user = await User.create({
+        fullname,
+        avatar: avatar.secure_url,
+        coverImage: coverImage?.secure_url || "",
+        email,
+        username: username.toLowerCase(),
+        password,
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if(!createdUser){
+        throw new apiError(500, "Error while creating user");
+    }
+    return res.status(201).json(
+        new apiResponse(
+            201,
+            createdUser,
+            "User registered successfully"
+        )
+    )
 })
 
-export {registerUser};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const loginUser = asyncHandler(async (req, res, next) =>{
+    return res.status(200).json({
+        success: true,
+        message: "User logged in successfully"
+    })
+})
+
+const logOutUser = asyncHandler(async (req, res, next)=>{
+    return res.status(200).json({
+        success: true,
+        message: "USer is logout successfully"
+    })
+})
+
+export {registerUser, loginUser , logOutUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -65,7 +65,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 userSchema.methods.generateAcessToken = function () {
     return jwt.sign(
         {
-            _Id: this._id,
+            _id: this._id,
             email: this.email,
             username: this.username,
             fullname: this.fullname
